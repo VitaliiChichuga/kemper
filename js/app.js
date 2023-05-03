249af35a4a677a2c3d24a9b8e7aa6d847434b428
VitaliiChichuga <chichygav@gmail.com>
2023-05-03 14:17:35 +0300

    add slider

--- js/app.js
+++ js/app.js
@@ -17,10 +17,10 @@ export const gallerySecond = [
 ];
 export const galleryThird = [
   {
-    src: 'https://funart.pro/uploads/posts/2021-04/1618343457_8-funart_pro-p-mashina-mechti-mashini-krasivo-foto-8.jpg',
+    src: 'https://scontent.fkbp1-1.fna.fbcdn.net/v/t39.30808-6/324512749_720153159499693_7051350553518913206_n.jpg?stp=dst-jpg_s640x640&_nc_cat=103&ccb=1-7&_nc_sid=a26aad&_nc_ohc=CA9dNrjUKX8AX_GFNT4&_nc_ht=scontent.fkbp1-1.fna&oh=00_AfDRDLzpW0GuuPAz3Fk-Oh2p3Sf91_xsAQ4ciR6jvFJSjg&oe=6455F18C',
   },
   {
-    src: 'https://auto.bigmir.net/i/52/42/01/8/5242018/6b6fef49df7c0c8abbe7caf149956b4b-quality_75Xresize_1Xallow_enlarge_0Xw_800Xh_0.jpg',
+    src: 'https://imageio.forbes.com/specials-images/imageserve/63da9376a84b26682d9afec2/0x0.jpg?format=jpg&width=1200',
   },
 ];
 export const galleryFourth = [
@@ -33,9 +33,9 @@ export const galleryFourth = [
 ];
 export const galleryFifth = [
   {
-    src: 'https://funart.pro/uploads/posts/2021-04/1618343457_8-funart_pro-p-mashina-mechti-mashini-krasivo-foto-8.jpg',
+    src: 'https://www.topgear.com/sites/default/files/news-listicle/image/2021/12/18.%20Koenigsegg%20Jesko.jpg?w=848&h=477',
   },
   {
-    src: 'https://auto.bigmir.net/i/52/42/01/8/5242018/6b6fef49df7c0c8abbe7caf149956b4b-quality_75Xresize_1Xallow_enlarge_0Xw_800Xh_0.jpg',
+    src: 'https://images.hindustantimes.com/auto/img/2021/09/13/600x338/bugatti_centodieci_windtunnel_02_1631514246365_1631514256968.jpg',
   },
 ];
--- js/gallery.js
+++ js/gallery.js
@@ -6,55 +6,59 @@ import {
   galleryFifth,
 } from './app.js';
 
-const refs = {
-  link: document.getElementById('first'),
-  link2: document.getElementById('second'),
-  link3: document.getElementById('third'),
-  link4: document.getElementById('fourth'),
-  link5: document.getElementById('fifth'),
-};
-
-function getValue(array, key) {
-  let res = [];
-  for (let i = 0; i < array.length; i++) {
-    res[array[i][key]] = array[i];
-  }
-  return res;
-}
-
-refs.link.addEventListener('click', event => {
-  event.preventDefault();
-
-  if (event.target.id === 'first') {
+const solutionsPageWork = document.querySelector('.solutions__page-work');
+solutionsPageWork.addEventListener('click', e => {
+  e.preventDefault;
+  console.log(e.target);
+  if (e.target.classList.contains('one')) {
     Fancybox.show(galleryFifth, 'src');
   }
-});
-
-refs.link2.addEventListener('click', event => {
-  event.preventDefault();
-
-  if (event.target.id === 'second') {
+  if (e.target.classList.contains('two')) {
     Fancybox.show(gallerySecond, 'src');
   }
 });
-refs.link3.addEventListener('click', event => {
-  event.preventDefault();
 
-  if (event.target.id === 'third') {
-    Fancybox.show(galleryThird, 'src');
-  }
-});
-refs.link4.addEventListener('click', event => {
-  event.preventDefault();
+// const refs = {
+//   link: document.getElementById('first'),
+//   link2: document.getElementById('second'),
+//   link3: document.getElementById('third'),
+//   link4: document.getElementById('fourth'),
+//   link5: document.getElementById('fifth'),
+// };
 
-  if (event.target.id === 'fourth') {
-    Fancybox.show(galleryFourth, 'src');
-  }
-});
-refs.link5.addEventListener('click', event => {
-  event.preventDefault();
+// refs.link.addEventListener('click', event => {
+//   event.preventDefault();
 
-  if (event.target.id === 'fifth') {
-    Fancybox.show(galleryFifth, 'src');
-  }
-});
+//   if (event.target.id === 'first') {
+//     Fancybox.show(galleryFifth, 'src');
+//   }
+// });
+
+// refs.link2.addEventListener('click', event => {
+//   event.preventDefault();
+
+//   if (event.target.id === 'second') {
+//     Fancybox.show(gallerySecond, 'src');
+//   }
+// });
+// refs.link3.addEventListener('click', event => {
+//   event.preventDefault();
+
+//   if (event.target.id === 'third') {
+//     Fancybox.show(galleryThird, 'src');
+//   }
+// });
+// refs.link4.addEventListener('click', event => {
+//   event.preventDefault();
+
+//   if (event.target.id === 'fourth') {
+//     Fancybox.show(galleryFourth, 'src');
+//   }
+// });
+// refs.link5.addEventListener('click', event => {
+//   event.preventDefault();
+
+//   if (event.target.id === 'fifth') {
+//     Fancybox.show(galleryFifth, 'src');
+//   }
+// });
